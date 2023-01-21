f2b4e7e8b9d02e7084f10e7e9dfb760854cf0c56
Justin <Justin@justinspicer.com>
2023-01-21 17:16:57 -0600

    fixed bad argument  bug

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-use rand::Rng;
+use rand::{Rng, rngs::ThreadRng};
 //use std::fs;
 
 //this is a joke choose your own adventure generator. It picks random words so you can choose the ones you like as you read and create your own adventure.
@@ -15,10 +15,12 @@ use rand::Rng;
 //output string
 
 fn main() {
+    //this seems messy. there has to be a better way.
+    let default_word_count = 3000;
     let args: Vec<String> = env::args().skip(1).collect();
     let word_count: usize = match args.first() {
-        Some(v) => v.parse().unwrap(),
-        None => 3000
+        Some(arg) => arg.parse().unwrap_or(default_word_count),
+        None => default_word_count
     };
 
     //replace with read from files
@@ -27,7 +29,7 @@ fn main() {
 
     let mut buffer:Vec<String> = Vec::with_capacity(word_count);
 
-    let mut rng = rand::thread_rng();
+    let mut rng: ThreadRng = rand::thread_rng();
 
     for _i in 1..word_count {
         let rnum: u32 = rng.gen_range(0..100);
